9b7d89c938db508431ba28302618fc536f2dc2e5
CharlesD1ing <dingyulu0622@hotmail.com>
2018-07-28 17:12:19 -0400

    finished ext2_ls

--- a3/ext2_helper.h
+++ a3/ext2_helper.h
@@ -11,9 +11,10 @@
 
 List* split_path(char* path) {
     List* tokens = makeList(5);
+    listAppend(tokens, ".");
 
     char* token;
-    token = strtok(path, "/");;
+    token = strtok(path, "/");
     while (token != NULL) {
         listAppend(tokens, token);
         token = strtok(NULL, "/");
@@ -29,7 +30,7 @@ typedef unsigned int one_index;
 //region Load disk to memory
 
 unsigned char* load_disk_to_mem(int file_descriptor) {
-    unsigned char* disk = mmap(NULL, 128*1024, PROT_READ | PROT_WRITE, MAP_SHARED, file_descriptor, 0);
+    unsigned char* disk = mmap(NULL, 128 * 1024, PROT_READ | PROT_WRITE, MAP_SHARED, file_descriptor, 0);
     if (disk == MAP_FAILED) {
         perror("mmap");
         exit(1);
@@ -52,67 +53,77 @@ unsigned char* get_block(unsigned char* disk, one_index block_ind) {
     return (disk + (EXT2_BLOCK_SIZE * block_ind));
 }
 
-struct ext2_super_block* get_super_block(unsigned char* disk){
+struct ext2_super_block* get_super_block(unsigned char* disk) {
     return (struct ext2_super_block*) get_block(disk, 1);
 }
 
-struct ext2_group_desc* get_group_descriptor(unsigned char* disk){
+struct ext2_group_desc* get_group_descriptor(unsigned char* disk) {
     return (struct ext2_group_desc*) get_block(disk, 2);
 }
 
-unsigned char* get_block_bitmap(unsigned char* disk){
+unsigned char* get_block_bitmap(unsigned char* disk) {
     struct ext2_group_desc* group_desc = get_group_descriptor(disk);
     return get_block(disk, group_desc->bg_block_bitmap);
 }
 
-unsigned char* get_inode_bitmap(unsigned char* disk){
+unsigned char* get_inode_bitmap(unsigned char* disk) {
     struct ext2_group_desc* group_desc = get_group_descriptor(disk);
     return get_block(disk, group_desc->bg_inode_bitmap);
 }
 
-struct ext2_inode* get_inode_table(unsigned char* disk){
+struct ext2_inode* get_inode_table(unsigned char* disk) {
     struct ext2_group_desc* group_desc = get_group_descriptor(disk);
-    return (struct ext2_inode *) get_block(disk, group_desc->bg_inode_table);
+    return (struct ext2_inode*) get_block(disk, group_desc->bg_inode_table);
 }
 
-struct ext2_inode* get_inode(unsigned char* disk, zero_index inode_ind){
+struct ext2_inode* get_inode(unsigned char* disk, zero_index inode_ind) {
     return &get_inode_table(disk)[inode_ind];
 }
 
-struct ext2_inode* get_root_inode(unsigned char* disk){
+struct ext2_inode* get_root_inode(unsigned char* disk) {
     // Handout specifies that root inode is at index 1
     return get_inode(disk, 1);
 }
 
-struct ext2_inode* get_inode_from_entry(unsigned char* disk, struct ext2_dir_entry_2* entry){
-    if (entry == NULL){
+struct ext2_inode* get_inode_from_entry(unsigned char* disk, struct ext2_dir_entry_2* entry) {
+    if (entry == NULL) {
         return NULL;
     }
     return get_inode(disk, entry->inode - 1);
 }
 
-struct ext2_dir_entry_2* get_next_dir_entry(struct ext2_dir_entry_2* entry){
-    return entry + (entry->rec_len);
+struct ext2_dir_entry_2* get_next_dir_entry(struct ext2_dir_entry_2* entry) {
+    char* byte_wise = (char*) entry;
+    char* shifted = byte_wise + entry->rec_len;
+
+    return (struct ext2_dir_entry_2*) shifted;
 }
 
-bool is_valid_dir_entry(struct ext2_dir_entry_2* start, struct ext2_dir_entry_2* current){
-    return (current - start) <= EXT2_BLOCK_SIZE;
+bool is_valid_dir_entry(struct ext2_dir_entry_2* start, struct ext2_dir_entry_2* current) {
+
+    unsigned long distance = ((char* ) current) - ((char*) start);
+
+    return (distance < EXT2_BLOCK_SIZE) &&
+           (current->rec_len > 0);
 }
 
 //endregion
 
 //region Predicates
 
-bool is_file(struct ext2_dir_entry_2* dir){
-    return dir->file_type == EXT2_FT_REG_FILE;
+bool is_file(struct ext2_dir_entry_2* dir) {
+    return (dir != NULL) &&
+            dir->file_type == EXT2_FT_REG_FILE;
 }
 
-bool is_directory(struct ext2_dir_entry_2* dir){
-    return dir->file_type == EXT2_FT_DIR;
+bool is_directory(struct ext2_dir_entry_2* dir) {
+    return (dir != NULL) &&
+            (dir->file_type == EXT2_FT_DIR);
 }
 
-bool is_link(struct ext2_dir_entry_2* dir){
-    return dir->file_type == EXT2_FT_SYMLINK;
+bool is_link(struct ext2_dir_entry_2* dir) {
+    return (dir != NULL) &&
+            dir->file_type == EXT2_FT_SYMLINK;
 }
 
 
@@ -120,13 +131,29 @@ bool is_link(struct ext2_dir_entry_2* dir){
 //endregion
 
 
+bool entry_name_comparison(struct ext2_dir_entry_2* dir, char* name){
+
+    if (dir->name_len != strlen(name)) {
+        return false;
+    }
+
+    for (int i = 0; i < dir->name_len; i++){
+        if (dir->name[i] != name[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 // Finds the directory entry in the data block starting at start,
 // whose file name is equal to name
 // If no such entry exists, return NULL
-struct ext2_dir_entry_2* block_matching_dir(struct ext2_dir_entry_2* start, char* name){
-    for (struct ext2_dir_entry_2* cur = start; is_valid_dir_entry(start, cur); get_next_dir_entry(cur)){
+struct ext2_dir_entry_2* block_matching_entry(struct ext2_dir_entry_2* start, char* name) {
+
+    for (struct ext2_dir_entry_2* cur = start; is_valid_dir_entry(start, cur); cur = get_next_dir_entry(cur)) {
         // TODO : THIS NAME COMPARISON MAY BE INCORRECT
-        if (is_directory(cur) && (strcmp(cur->name, name) == 0)){
+        if (entry_name_comparison(cur, name)) {
             return cur;
         }
     }
@@ -135,44 +162,45 @@ struct ext2_dir_entry_2* block_matching_dir(struct ext2_dir_entry_2* start, char
 
 
 // Finds the directory entry in inode whose name is name
-struct ext2_dir_entry_2* find_matching_dir(unsigned char* disk, struct ext2_inode* inode, char* name){
+struct ext2_dir_entry_2* find_matching_entry(unsigned char* disk, struct ext2_inode* inode, char* name) {
+    if (name == NULL) return NULL;
 
     // Look through direct data blocks (0 to 11 inclusive)
-    for (int i = 0; i <= 11; i++){
+    for (int i = 0; i <= 11; i++) {
         one_index block_num = inode->i_block[i];
 
         // Skip absent blocks if there is no block
-        if (block_num == 0){
+        if (block_num == 0) {
             continue;
         }
 
         // Get the first directory entry, and look for a matching directory
         struct ext2_dir_entry_2* first_entry = (struct ext2_dir_entry_2*) get_block(disk, block_num);
-        struct ext2_dir_entry_2* match = block_matching_dir(first_entry, name);
-        if (match != NULL){
+        struct ext2_dir_entry_2* match = block_matching_entry(first_entry, name);
+        if (match != NULL) {
             return match;
         }
     }
 
     // Skip absent indirect block
-    if (inode->i_block[12] == 0){
+    if (inode->i_block[12] == 0) {
         return NULL;
     }
 
     // Look through indirect data block for matching directory
     one_index* block_numbers = (one_index*) get_block(disk, inode->i_block[12]);
-    for (int i = 0; i < EXT2_BLOCK_SIZE; i++){
+    for (int i = 0; i < EXT2_BLOCK_SIZE; i++) {
         one_index block_num = block_numbers[i];
 
         // Skip absent blocks if there is no block
-        if (block_num == 0){
+        if (block_num == 0) {
             continue;
         }
 
         // Get the first directory entry, and look for a matching directory
         struct ext2_dir_entry_2* first_entry = (struct ext2_dir_entry_2*) get_block(disk, block_num);
-        struct ext2_dir_entry_2* match = block_matching_dir(first_entry, name);
-        if (match != NULL){
+        struct ext2_dir_entry_2* match = block_matching_entry(first_entry, name);
+        if (match != NULL) {
             return match;
         }
     }
@@ -180,33 +208,56 @@ struct ext2_dir_entry_2* find_matching_dir(unsigned char* disk, struct ext2_inod
     return NULL;
 }
 
-void print_block_contents(struct ext2_dir_entry_2* start){
-    for (struct ext2_dir_entry_2* cur = start; is_valid_dir_entry(start, cur); get_next_dir_entry(cur)){
+
+
+void print_dir_name(struct ext2_dir_entry_2* dir){
+
+    // Print each character up to name_len
+    for (int u = 0; u < dir->name_len; u++) {
+        printf("%c", dir->name[u]);
+    }
+
+    // Finish with newline
+    printf("\n");
+}
+
+void print_block_contents(struct ext2_dir_entry_2* start, bool show_dots) {
+    for (struct ext2_dir_entry_2* cur = start; is_valid_dir_entry(start, cur); cur = get_next_dir_entry(cur)) {
+
+        // Skip over dots if asked
+        if (!show_dots) {
+            if ((strcmp(cur->name, ".") == 0) || (strcmp(cur->name, "..") == 0)) {
+                continue;
+            }
+        }
+
         // TODO THIS ASSUMES CUR IS PROPER
-        printf("%s\n", cur->name);
+        if (cur->name_len != 0){
+            print_dir_name(cur);
+        }
     }
 }
 
-void print_dir_contents(char* disk, struct ext2_dir_entry_2* entry){
+void print_dir_contents(unsigned char* disk, struct ext2_dir_entry_2* entry, bool show_dots) {
 
     struct ext2_inode* inode = get_inode_from_entry(disk, entry);
 
     // Look through direct data blocks (0 to 11 inclusive)
-    for (int i = 0; i <= 11; i++){
+    for (int i = 0; i <= 11; i++) {
         one_index block_num = inode->i_block[i];
 
         // Skip absent blocks if there is no block
-        if (block_num == 0){
+        if (block_num == 0) {
             continue;
         }
 
         // Get the first directory entry, and print the contents
         struct ext2_dir_entry_2* first_entry = (struct ext2_dir_entry_2*) get_block(disk, block_num);
-        print_block_contents(first_entry);
+        print_block_contents(first_entry, show_dots);
     }
 
     // Skip absent indirect block
-    if (inode->i_block[12] == 0){
+    if (inode->i_block[12] == 0) {
         return;
     }
 
@@ -222,34 +273,32 @@ void print_dir_contents(char* disk, struct ext2_dir_entry_2* entry){
 
         // Get the first directory entry, and print the contents
         struct ext2_dir_entry_2* first_entry = (struct ext2_dir_entry_2*) get_block(disk, block_num);
-        print_block_contents(first_entry);
+        print_block_contents(first_entry, show_dots);
     }
 }
 
-int ls_proto(unsigned char* disk, char* path){
-    List* path_components = split_path(path);
+struct ext2_dir_entry_2* traverse_path(unsigned char* disk, List* path_components) {
 
-    struct ext2_inode* cur = get_root_inode(disk);
-    struct ext2_dir_entry_2* entry;
-    zero_index ind = 0;
+    struct ext2_dir_entry_2* entry = NULL;
+    struct ext2_inode* entry_inode = get_root_inode(disk);
+    for (zero_index ind = 0; ind < path_components->count; ind++) {
 
-    while (cur != NULL){
-        entry = find_matching_dir(disk, cur, path_components->contents[ind]);
-        cur = get_inode_from_entry(disk, entry);
-        ind++;
+        // Try to find the matching entry, abort if not found
+        entry = find_matching_entry(disk, entry_inode, path_components->contents[ind]);
+        if (entry == NULL) {
+            break;
+        }
+
+        // If entry is not a directory, abort - we can't traverse into it
+        if (!is_directory(entry)){
+            break;
+        }
+
+        // Update inode
+        entry_inode = get_inode_from_entry(disk, entry);
     }
 
-    if (ind != path_components->count - 1) {
-        printf("%s\n", "No such file or directory");
-        return ENOENT;
-    }
-
-    if (is_file(entry) || is_link(entry)) {
-        printf("%s\n", listPeek(path_components));
-    }
-    else if (is_directory(entry)) {
-        print_dir_contents(disk, entry);
-    }
-
+    return entry;
 }
+
 
--- a3/ext2_ls.c
+++ a3/ext2_ls.c
@@ -1,11 +1,76 @@
 #include "ext2_helper.h"
 #include<fcntl.h>
+#include <getopt.h>
 
 
+int ext2_ls(unsigned char* disk, char* path, bool show_dots) {
+    List* path_components = split_path(path);
 
+    struct ext2_dir_entry_2* entry = traverse_path(disk, path_components);
+    if (entry == NULL) {
+        printf("%s\n", "No such file or directory");
+        return ENOENT;
+    }
+
+    if (is_file(entry) || is_link(entry)) {
+        printf("%s\n", (char*) listPeek(path_components));
+    } else if (is_directory(entry)) {
+        print_dir_contents(disk, entry, show_dots);
+    }
+
+    destroyList(path_components);
+
+    return 0;
+
+}
+
+void crash_with_usage(char* prog_name){
+    fprintf(stderr, "Usage: %s [disk] [option -a] [path]\n", prog_name);
+    exit(1);
+}
 
 int main(int argc, char* argv[]) {
-    int file_descriptor = open(argv[1], O_RDWR);
+    int ch = 0;
+    bool show_dots = false;
+
+    char* img = argv[1];
+    char* path;
+
+    switch(argc) {
+        case 3:
+            if (strcmp(argv[2], "-a") == 0) {
+                crash_with_usage(argv[0]);
+            }
+            path = argv[2];
+            break;
+        case 4:
+            path = argv[3];
+            break;
+        default:
+            crash_with_usage(argv[0]);
+    }
+
+
+    while ((ch = getopt(argc, argv, "a")) != -1) {
+        switch(ch) {
+            case 'a':
+                // option to print everything
+                show_dots = true;
+                break;
+            default:
+                crash_with_usage(argv[0]);
+        }
+    }
+
+
+    int file_descriptor = open(img, O_RDWR);
+
+    // Opening disk
+    if (!file_descriptor) {
+        fprintf(stderr, "Disk image '%s' not found.", img);
+        exit(ENOENT);
+    }
+
     unsigned char* disk = load_disk_to_mem(file_descriptor);
-    ls_proto(disk, argv[2]);
+    ext2_ls(disk, path, show_dots);
 }
